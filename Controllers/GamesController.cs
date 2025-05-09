eac1b7e0428fa5c2d70574314cdc04fa6a6d039a
George Rougalas <georgios.rougalas@gmail.com>
2025-05-09 20:14:48 +0300

    Pre-merge Catch up #1
    Fill blank games and various mini updates
    Database pulling for Game Controller is due tomorrow
    Drop & Drag upgrade to Word Match / user friendly js is due Sunday

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -120,11 +120,73 @@ namespace ProjectSpetses.Controllers
             //the WordMatch page doesn't save the data in any form yet (use asp-for= and a model)
             return null;
         }
+        public IActionResult FillBlank()
+        {
+            var quiz_items = GetBlanks();
+            return View(quiz_items);
+        }
+        private List<Blank_item> GetBlanks()
+        {
+            //This is where you do DB stuff
+            //Blank items are near identical to quiz items, when DB is developed we should consider merging these
+
+            //Testing data
+            List<Blank_item> quiz_items = new List<Blank_item> {
+                new Blank_item
+            {
+                Section_Id = Guid.NewGuid(),
+                Description1 = "Oranges are a ",
+                Description2 = "fruit?",
+                Solution = "yummy",
+                Answers = new List<string>() { "gross", "red", "blue", "unhealthy" }
+            },
+                new Blank_item
+            {
+                Section_Id = Guid.NewGuid(),
+                Description1 = "Italians live in ",
+                Description2 = " city?",
+                Solution = "Rome",
+                Answers = new List<string>() { "Athens", "Rome", "Napoli", "Heraklion" }
+            },
+                new Blank_item
+            {
+                Section_Id = Guid.NewGuid(),
+                Description1 = "Oranges are a ",
+                Description2 = "fruit?",
+                Solution = "Cairo",
+                Answers = new List<string>() { "Athens", "Nile", "Patras", "Cairo" }
+            },
+                new Blank_item
+            {
+                Section_Id = Guid.NewGuid(),
+                Description1 = "Oranges are a ",
+                Description2 = "fruit?",
+                Solution = "London",
+                Answers = new List<string>() { "London", "Dublin", "Patras", "Heraklion" }
+            },
+                new Blank_item
+            {
+                Section_Id = Guid.NewGuid(),
+                Description1 = "Oranges are a ",
+                Description2 = "fruit?",
+                Solution = "London",
+                Answers = new List<string>() { "London", "Dublin", "Patras", "Heraklion" }
+            }
+            };
+            //End of Testing data
+
+            //this shuffles the answers on the quiz, in case it isn't already done in the DB
+            for (int i = 0; i < quiz_items.Count; i++)
+            {
+                quiz_items[i].Answers = ShuffleList(quiz_items[i].Answers);
+            }
+
+            return quiz_items;
+        }
         public static List<T> ShuffleList<T>(List<T> list)
         {//this shuffles String lists to avoid obvious tests
             Random random = new Random();
             return list.OrderBy(_ => random.Next()).ToList();
         }
-
     }
 }
