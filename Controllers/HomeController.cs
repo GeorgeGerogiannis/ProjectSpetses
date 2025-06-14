d5cc04255024f9656475e7ed2d024f0317a59228
GeorgeGerogiannis <georgegerogiannis2003@gmail.com>
2025-06-15 00:24:33 +0300

    Games Front-end
    - Improved appearance for all games and results page
    - Removed Privacy page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -64,11 +64,6 @@ namespace ProjectSpetses.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
